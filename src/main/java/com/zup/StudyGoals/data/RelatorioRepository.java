363c0df73ae7f5de12f15039315ad7a8fb539a86
João Vitor Cunha Assumpção <joao.assumpcao@zup.com.br>
2023-10-05 15:05:03 -0300

    RelatorioRepository Criado

--- src/main/java/com/zup/StudyGoals/data/RelatorioRepository.java
+++ src/main/java/com/zup/StudyGoals/data/RelatorioRepository.java
@@ -1,4 +1,9 @@
 package com.zup.StudyGoals.data;
 
-public interface RelatorioRepository {
+import com.zup.StudyGoals.domain.Relatorio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RelatorioRepository extends JpaRepository<Relatorio, Long> {
 }
