b61712b542d5ac085876da6572bf05cb2226b720
Giorgia Santos <giorgia.santos@zup.com.br>
2023-10-05 18:19:41 -0300

    Adicionando modificações no service de Meta

--- src/main/java/com/zup/StudyGoals/application/Metaservice.java
+++ src/main/java/com/zup/StudyGoals/application/Metaservice.java
@@ -1,12 +1,12 @@
 package com.zup.StudyGoals.application;
-import com.zup.StudyGoals.data.MaterialDeEstudoRepository;
+
+import com.zup.StudyGoals.application.mapper.MetaDTOMapper;
 import com.zup.StudyGoals.data.MetaRepository;
-import com.zup.StudyGoals.domain.MaterialDeEstudo;
+
 import com.zup.StudyGoals.domain.Meta;
 import com.zup.StudyGoals.dto.MetaDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,9 +17,6 @@ public class MetaService {
 
     @Autowired
     MetaRepository metaRepository;
-
-    @Autowired
-    MaterialDeEstudoRepository materialDeEstudoRepository;
 
     // método para listar todas as metas
     public List<MetaDTO> listarMetas(){
@@ -42,16 +39,11 @@ public class MetaService {
         return Optional.empty();
     }
 
-
     // método para inserir uma nova meta
-    @Transactional
-    public void adicionarNovaMetaComMateriais(Meta meta, List<MaterialDeEstudo> materiaisDeEstudo) {
-        Meta novaMeta = metaRepository.save(meta);
-
-        for (MaterialDeEstudo material : materiaisDeEstudo) {
-            novaMeta.adiciona(material);
-            materialDeEstudoRepository.save(material);
-        }
+    public MetaDTO cadastrarMeta(MetaDTO metaDTO){
+        Meta novaMeta = MetaDTOMapper.INSTANCE.DTOParaMeta(metaDTO);
+        metaRepository.save(novaMeta);
+        return new MetaDTO(novaMeta);
     }
 
     // método para editar metas
@@ -72,9 +64,6 @@ public class MetaService {
         }
         if(meta != null){
             meta.setObjetivo(metas.getObjetivo());
-        }
-        if(meta != null) {
-            meta.setMateriaisDeEstudo(metas.getMateriaisDeEstudo());
         }
 
         return metaRepository.save(meta);
