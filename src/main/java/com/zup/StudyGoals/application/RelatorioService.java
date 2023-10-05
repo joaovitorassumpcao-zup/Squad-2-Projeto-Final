5301ea12f45b6dcc7575ce1009d67ac4220992f9
João Vitor Cunha Assumpção <joao.assumpcao@zup.com.br>
2023-10-05 20:26:39 -0300

    Correções em RelatorioService

--- src/main/java/com/zup/StudyGoals/application/RelatorioService.java
+++ src/main/java/com/zup/StudyGoals/application/RelatorioService.java
@@ -1,13 +1,20 @@
 package com.zup.StudyGoals.application;
 
+import com.zup.StudyGoals.application.mapper.RelatorioDTOMapper;
 import com.zup.StudyGoals.data.RelatorioRepository;
 import com.zup.StudyGoals.domain.Relatorio;
+import com.zup.StudyGoals.dto.MetaDTO;
+import com.zup.StudyGoals.dto.RelatorioDTO;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.validation.constraints.NotNull;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class RelatorioService {
@@ -15,35 +22,56 @@ public class RelatorioService {
     @Autowired
     RelatorioRepository relatorioRepository;
 
-    public List<Relatorio> listarRelatorios() {
-        return relatorioRepository.findAll();
+    public List<RelatorioDTO> listarRelatorios() {
+        return relatorioRepository
+                .findAll()
+                .stream()
+                .map(RelatorioDTOMapper.INSTANCE::relatorioParaDTO)
+                .collect(Collectors.toList());
     }
 
-    public Optional<Relatorio> buscarRelatorioPorId(Long id) {
-        return relatorioRepository.findById(id);
+    public Optional<RelatorioDTO> buscarRelatorioPorId(Long id) {
+        return relatorioRepository.findById(id)
+                .map(RelatorioDTOMapper.INSTANCE::relatorioParaDTO);
     }
 
-    public Relatorio cadastrarRelatorio(Relatorio relatorio) throws Exception {
-        Optional<Relatorio> relatorioOptional = relatorioRepository
-                .findById(relatorio.getId());
-        if (relatorioOptional.isPresent()) throw new Exception("Relatório já existe!");
-        else return relatorioRepository.save(relatorio);
+    public RelatorioDTO cadastrarRelatorio(RelatorioDTO relatorioDTO) {
+        //Optional<Relatorio> relatorioOptional = relatorioRepository
+        //        .findById(relatorioDTO.getId());
+        //if (relatorioOptional.isPresent()) return new Error();
+        //else {
+            relatorioRepository
+                    .save(RelatorioDTOMapper.INSTANCE.DTOParaRelatorio(relatorioDTO));
+            return relatorioDTO;
+        //}
     }
 
-    public Relatorio editarRelatorio(Long id, Relatorio relatorioAlterado) throws Exception {
+    public Optional<RelatorioDTO> alterarRelatorio(Long id, RelatorioDTO relatorioDTO) {
         Optional<Relatorio> relatorioOptional = relatorioRepository.findById(id);
 
-        if (relatorioOptional.isPresent()){
+        if (relatorioOptional.isPresent()) {
             Relatorio relatorio = relatorioOptional.get();
 
-            BeanUtils.copyProperties(relatorioAlterado, relatorio, "id");
+            BeanUtils.copyProperties(relatorioDTO, relatorio, "id");
+            relatorioRepository.save(relatorio);
 
-            return relatorioRepository.save(relatorio);
+            return Optional.of(RelatorioDTOMapper.INSTANCE.relatorioParaDTO(relatorio));
         }
-        else throw new Exception("Relatório não existe!");
+        else return Optional.empty();
     }
 
-    public void excluirRelatorioPorId(Long id) {
+    public void deletarRelatorio(Long id) {
         relatorioRepository.deleteById(id);
+    }
+
+    public Boolean metaFoiConcluida(MetaDTO metaDTO) {
+        if (metaDTO.getDataFinal().isBefore(LocalDateTime.now())) return Boolean.TRUE;
+        else return Boolean.FALSE;
+    }
+
+    public Long calcularDiasParaMeta(MetaDTO metaDTO) {
+        Duration duracao = Duration
+                .between(LocalDateTime.now(), metaDTO.getDataFinal());
+        return duracao.toDays();
     }
 }
