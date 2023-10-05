0ca680a5aeec1c9fa72b85c2ae4dc2684b85d6c2
Giorgia Santos <giorgia.santos@zup.com.br>
2023-10-05 14:43:35 -0300

    Subindo DTO de MaterialDeEstudo e a adaptação de Service com o DTO

--- src/main/java/com/zup/StudyGoals/dto/MaterialDeEstudoDTO.java
+++ src/main/java/com/zup/StudyGoals/dto/MaterialDeEstudoDTO.java
@@ -1,28 +1,35 @@
 package com.zup.StudyGoals.dto;
 
 import com.zup.StudyGoals.domain.Categoria;
-import lombok.AccessLevel;
+import com.zup.StudyGoals.domain.MaterialDeEstudo;
 import lombok.Data;
+import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
-public @Data class MaterialDeEstudoDTO implements Serializable {
+@Getter
+@Setter
+public class MaterialDeEstudoDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String titulo;
+    private Categoria categoria;
+    private String url;
+    private String resumo;
+    private LocalDateTime dataInicio;
+    private LocalDateTime dataConclusao;
 
-    @Setter(AccessLevel.NONE)
-    Long id;
+    public MaterialDeEstudoDTO (MaterialDeEstudo materialDeEstudo){
+        this.titulo = materialDeEstudo.getTitulo();
+        this.categoria = materialDeEstudo.getCategoria();
+        this.url = materialDeEstudo.getUrl();
+        this.resumo = materialDeEstudo.getResumo();
+        this.dataInicio = materialDeEstudo.getDataInicio();
+        this.dataConclusao = materialDeEstudo.getDataConclusao();
+    }
 
-    String titulo;
+    public MaterialDeEstudoDTO (){
 
-    Categoria categoria;
-
-    String url;
-
-    String resumo;
-
-    LocalDateTime dataInicio;
-
-    LocalDateTime dataConclusao;
-
+    }
 }
