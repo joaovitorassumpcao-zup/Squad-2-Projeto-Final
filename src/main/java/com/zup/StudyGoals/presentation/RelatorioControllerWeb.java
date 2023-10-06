d6220f7b30be81b27eac00fd36dd321a4529882d
Elizabeth Oliveira <elizabeth.oliveira@zup.com.br>
2023-10-06 14:45:26 -0300

    Controller metaFoiConcluida

--- src/main/java/com/zup/StudyGoals/presentation/RelatorioControllerWeb.java
+++ src/main/java/com/zup/StudyGoals/presentation/RelatorioControllerWeb.java
@@ -1,6 +1,8 @@
 package com.zup.StudyGoals.presentation;
 
+import com.zup.StudyGoals.application.MetaService;
 import com.zup.StudyGoals.application.RelatorioService;
+import com.zup.StudyGoals.dto.MetaDTO;
 import com.zup.StudyGoals.dto.RelatorioDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @RestController
@@ -16,6 +19,9 @@ public class RelatorioControllerWeb {
 
     @Autowired
     RelatorioService relatorioService;
+
+    @Autowired
+    MetaService metaService;
 
     @GetMapping
     public ResponseEntity<List<RelatorioDTO>> listarRelatorios() {
@@ -52,6 +58,22 @@ public class RelatorioControllerWeb {
     public ResponseEntity<?> deletarRelatorio(@PathVariable Long id) {
         relatorioService.deletarRelatorio(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/metaconcluida/{id}")
+    public ResponseEntity<?> metaFoiConcluida(@PathVariable Long id){
+        Optional<MetaDTO> metaDTOOptional =
+                metaService.buscarMetaPorId(id);
+
+        if (metaDTOOptional.isPresent()) {
+
+            MetaDTO metaDTO = metaDTOOptional.orElseThrow(() -> new NoSuchElementException("Optional está vazio"));
+            return ResponseEntity.ok(relatorioService.metaFoiConcluida(metaDTO));
+
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Meta não encontrada.");
+        }
     }
 
 }
