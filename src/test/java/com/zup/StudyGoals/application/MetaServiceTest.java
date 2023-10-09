d47a3bd13dfb1e0c45215c5477861c1fbfbf4d1b
Giorgia Santos <giorgia.santos@zup.com.br>
2023-10-09 16:15:46 -0300

    Adicionando teste unitário do método de buscar meta por id

--- src/test/java/com/zup/StudyGoals/application/MetaServiceTest.java
+++ src/test/java/com/zup/StudyGoals/application/MetaServiceTest.java
@@ -5,6 +5,9 @@ import com.zup.StudyGoals.domain.Categoria;
 import com.zup.StudyGoals.domain.MaterialDeEstudo;
 import com.zup.StudyGoals.domain.Meta;
 import com.zup.StudyGoals.dto.MetaDTO;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
@@ -16,10 +19,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 @ExtendWith(MockitoExtension.class)
@@ -31,10 +34,18 @@ class MetaServiceTest {
     @Mock
     private MetaRepository metaRepository;
 
+    private Meta meta;
+
+    @Before
+    public void setUp() {
+        metaService = new MetaService();
+        meta = new Meta(1L, "Inglês", LocalDateTime.parse("2023-10-20T08:00:00"), LocalDateTime.parse("2023-10-20T08:00:00") , 30, "Melhorar gramática", null);
+    }
+
     @Test
     void TesteListarMetas() throws Exception{
 
-        MaterialDeEstudo materialDeEstudo = new MaterialDeEstudo(1L, "Verbo to be", Categoria.VIDEO, "https://www.youtube.com", "Lorem ipsum",LocalDateTime.parse("2023-10-20T08:00:00"), LocalDateTime.parse("2023-10-20T08:00:00"), null);
+        MaterialDeEstudo materialDeEstudo = new MaterialDeEstudo(1L, "Verbo to be", Categoria.VIDEO, "https://www.youtube.com", "Lorem ipsum",LocalDateTime.parse("2023-10-20T08:00:00"), LocalDateTime.parse("2023-10-20T08:00:00"),meta);
         List<MaterialDeEstudo> listaMaterial = new ArrayList<>();
         listaMaterial.add(materialDeEstudo);
 
@@ -56,17 +67,40 @@ class MetaServiceTest {
     }
 
     @Test
-    void buscarMetaPorId() throws Exception{
+    void testeBuscarMetaPorId() throws Exception{
+
+        MaterialDeEstudo materialDeEstudo = new MaterialDeEstudo(1L, "Verbo to be", Categoria.VIDEO, "https://www.youtube.com", "Lorem ipsum",LocalDateTime.parse("2023-10-20T08:00:00"), LocalDateTime.parse("2023-10-20T08:00:00"),meta);
+        List<MaterialDeEstudo> listaMaterial = new ArrayList<>();
+        listaMaterial.add(materialDeEstudo);
+
+        Meta meta =  new Meta( 1L, "Inglês", LocalDateTime.parse("2023-10-20T08:00:00"), LocalDateTime.parse("2023-10-20T08:00:00") , 30, "Melhorar gramática", listaMaterial);
+
+        when(metaRepository.findById(1L)).thenReturn(Optional.of(meta));
+
+        Optional<MetaDTO> resultado = metaService.buscarMetaPorId(1L);
+        verify(metaRepository, times(1)).findById(1L);
+
+        assertTrue(resultado.isPresent());
+        assertEquals(meta.getAssunto(),resultado.get().getAssunto());
+        assertEquals(meta.getDataDeInicio(), resultado.get().getDataDeInicio());
+        assertEquals(meta.getDataFinal(),resultado.get().getDataFinal());
+        assertEquals(meta.getMetaMinutosDia(),resultado.get().getMetaMinutosDia());
+        assertEquals(meta.getObjetivo(),resultado.get().getObjetivo());
+        assertEquals(meta.getMateriaisDeEstudo(),resultado.get().getMateriaisDeEstudo());
+
     }
 
+    @Disabled
     @Test
     void adicionarNovaMetaComMateriais() throws Exception{
     }
 
+    @Disabled
     @Test
     void editarMeta() throws Exception{
     }
 
+    @Disabled
     @Test
     void deletarMeta() throws Exception{
     }
