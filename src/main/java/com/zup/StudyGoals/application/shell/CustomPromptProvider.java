64c2939cfd7e71f2977419f7d277d1b175668d6a
João Vitor Cunha Assumpção <joao.assumpcao@zup.com.br>
2023-10-09 19:08:04 -0300

    Prompt e Classe para comandos eventuais

---
+++ src/main/java/com/zup/StudyGoals/application/shell/CustomPromptProvider.java
@@ -0,0 +1,14 @@
+package com.zup.StudyGoals.application.shell;
+
+import org.jline.utils.AttributedString;
+import org.springframework.shell.jline.PromptProvider;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomPromptProvider implements PromptProvider {
+
+    @Override
+    public AttributedString getPrompt() {
+        return new AttributedString(" >>> ");
+    }
+}
---
+++ src/main/java/com/zup/StudyGoals/application/shell/ShellCommand.java
@@ -0,0 +1,7 @@
+package com.zup.StudyGoals.application.shell;
+
+import org.springframework.shell.standard.ShellCommandGroup;
+
+@ShellCommandGroup
+public class ShellCommand {
+}
