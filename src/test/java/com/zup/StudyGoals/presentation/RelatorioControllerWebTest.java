cc5663eaaf8109eb3bdd24c10ccb686730019734
Giorgia Santos <giorgia.santos@zup.com.br>
2023-10-13 13:48:32 -0300

    Adicionando configurações da classe de testes de relatorio

--- src/test/java/com/zup/StudyGoals/presentation/RelatorioControllerWebTest.java
+++ src/test/java/com/zup/StudyGoals/presentation/RelatorioControllerWebTest.java
@@ -1,16 +1,36 @@
 package com.zup.StudyGoals.presentation;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zup.StudyGoals.application.RelatorioService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@RunWith(SpringRunner.class)
+@WebMvcTest(RelatorioControllerWebTest.class)
 class RelatorioControllerWebTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private RelatorioService relatorioService;
+
+
     @Test
-    void listarRelatorios() {
+    void listarRelatorios() throws Exception{
     }
 
     @Test
-    void cadastrarRelatorio() {
+    void cadastrarRelatorio() throws Exception{
     }
 }
