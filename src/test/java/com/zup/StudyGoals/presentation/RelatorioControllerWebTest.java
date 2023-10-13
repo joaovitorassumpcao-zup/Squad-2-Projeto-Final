a0013b6b7b0e18bc4816ecabd2f1f87287ed78a3
Giorgia Santos <giorgia.santos@zup.com.br>
2023-10-13 13:39:27 -0300

    Adicionando classe de testes integrados de relatorio

---
+++ src/test/java/com/zup/StudyGoals/presentation/RelatorioControllerWebTest.java
@@ -0,0 +1,16 @@
+package com.zup.StudyGoals.presentation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RelatorioControllerWebTest {
+
+    @Test
+    void listarRelatorios() {
+    }
+
+    @Test
+    void cadastrarRelatorio() {
+    }
+}
