4ddf665b4fbfa0e4e8b851b00bfc4560e25b50d8
João Vitor Cunha Assumpção <joao.assumpcao@zup.com.br>
2023-10-16 12:39:38 -0300

    Atributos para requests

--- src/main/java/com/zup/StudyGoals/presentation/apiclient/ApiClient.java
+++ src/main/java/com/zup/StudyGoals/presentation/apiclient/ApiClient.java
@@ -7,4 +7,9 @@ import okhttp3.Response;
 import java.io.IOException;
 
 public class ApiClient {
+
+    private static OkHttpClient client = new OkHttpClient();
+
+    private final String LOCAL_URL = "https://localhost:8080/api";
+
 }
