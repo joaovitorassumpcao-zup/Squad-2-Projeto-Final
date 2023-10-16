311b7f2b665f6fd6c344e8ff8c62b2348d596e82
João Vitor Cunha Assumpção <joao.assumpcao@zup.com.br>
2023-10-16 12:38:53 -0300

    Classe `ApiClient` para os métodos do OkHttp

---
+++ src/main/java/com/zup/StudyGoals/presentation/apiclient/ApiClient.java
@@ -0,0 +1,10 @@
+package com.zup.StudyGoals.presentation.apiclient;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class ApiClient {
+}
