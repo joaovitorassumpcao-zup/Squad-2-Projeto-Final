ec6d9c9a64e0d9a3b9252ce959ba0d401bf54863
Giorgia Santos <giorgia.santos@zup.com.br>
2023-10-17 21:11:33 -0300

    Atualizando view de deletar uma meta

--- src/main/java/com/zup/StudyGoals/view/DeletarMeta.java
+++ src/main/java/com/zup/StudyGoals/view/DeletarMeta.java
@@ -1,28 +1,55 @@
 package com.zup.StudyGoals.view;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.zup.StudyGoals.domain.MaterialDeEstudo;
+import com.zup.StudyGoals.domain.Meta;
 import com.zup.StudyGoals.presentation.apiclient.ApiClient;
+import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.List;
 
 public class DeletarMeta extends JFrame{
     private JButton deletarMetaButton;
     private ApiClient apiClient;
+    private ObjectMapper objectMapper;
     private JPanel deletarMeta;
     private JComboBox metas;
 
     public DeletarMeta() {
         this.apiClient = new ApiClient();
-        Long idMeta = pegarIdMeta();
+        this.objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
+        String response = null;
+        try {
+            response = apiClient.getRequest("/metas");
+            List<Meta> listaTodasAsMetas = objectMapper
+                    .readValue(
+                            response,
+                            TypeFactory.defaultInstance()
+                                    .constructCollectionType(List.class, Meta.class)
+                    );
+            preecherComboBox(listaTodasAsMetas);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         deletarMetaButton.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
+                Meta metaASerDeletada = (Meta) metas.getSelectedItem();
+                assert metaASerDeletada != null;
+                String metaId = metaASerDeletada.getId().toString();
                 try {
-                    apiClient.deleteRequest("/metas/" + idMeta);
+                    apiClient.deleteRequest("/metas/" + metaId);
                     JOptionPane.showMessageDialog(null, "Meta deletada com sucesso! ", "Meta deletada ",JOptionPane.INFORMATION_MESSAGE);
                 }catch (IOException exception){
                     exception.printStackTrace();
@@ -41,8 +68,7 @@ public class DeletarMeta extends JFrame{
         setTitle("Deletar meta");
         setVisible(true);
     }
-    private Long pegarIdMeta(){
-        String idMetaString = JOptionPane.showInputDialog("Digite o ID da meta que deseja editar: ");
-        return Long.parseLong(idMetaString);
+    private void preecherComboBox(@NotNull List<Meta> listaMetas) {
+        listaMetas.forEach(meta -> metas.addItem(meta.getAssunto()));
     }
 }
